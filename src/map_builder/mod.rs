c9ad9ebc63b48daeac313fc0c05df1a80b08c075
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-06 17:32:58 +0100

    Added new map generators and basic prefab support

---
+++ src/map_builder/mod.rs
@@ -0,0 +1,88 @@
+mod automata;
+mod drunkard;
+mod empty;
+mod prefab;
+mod rooms;
+
+use crate::prelude::*;
+use automata::CellularAutomataArchitect;
+use drunkard::DrunkardWalkArchitect;
+use rooms::RoomsArchitect;
+
+use self::prefab::apply_prefab;
+
+trait MapArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub monster_spawns: Vec<Point>,
+    pub player_start: Point,
+    pub amulet_start: Point,
+}
+
+impl MapBuilder {
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {}),
+        };
+        let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
+        mb
+    }
+
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+    }
+
+    fn find_most_distance(&self) -> Point {
+        let dijkstra_map = DijkstraMap::new(
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            &vec![self.map.point2d_to_index(self.player_start)],
+            &self.map,
+            1024.0,
+        );
+
+        const UNREACHABLE: &f32 = &f32::MAX;
+        self.map.index_to_point2d(
+            dijkstra_map
+                .map
+                .iter()
+                .enumerate()
+                .filter(|(_, dist)| *dist < UNREACHABLE)
+                .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+                .unwrap()
+                .0,
+        )
+    }
+
+    fn spawn_monster(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        const NUM_MONSTERS: usize = 50;
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|(idx, t)| {
+                **t == TileType::Floor
+                    && DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(*idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+        // TODO: We could shuffle spawnable_tiles and grab the first NUM_MONSTERS entries.
+        for _ in 0..NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+            spawns.push(spawnable_tiles[target_index].clone());
+            spawnable_tiles.remove(target_index);
+        }
+        spawns
+    }
+}
---
+++ src/map_builder/rooms.rs
@@ -0,0 +1,94 @@
+use super::MapArchitect;
+use crate::prelude::*;
+
+const NUM_ROOMS: usize = 20;
+
+pub struct RoomsArchitect {}
+
+impl RoomsArchitect {
+    fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator, mb: &mut MapBuilder) {
+        while mb.rooms.len() < NUM_ROOMS {
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_HEIGHT - 10),
+                rng.range(2, 10),
+                rng.range(2, 10),
+            );
+            let mut overlap = false;
+            for r in mb.rooms.iter() {
+                if r.intersect(&room) {
+                    overlap = true;
+                    break;
+                }
+            }
+            if !overlap {
+                room.for_each(|p| {
+                    // TODO: Use self.map.try_idx(p)?
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
+                        let idx = map_idx(p.x, p.y);
+                        mb.map.tiles[idx] = TileType::Floor;
+                    }
+                });
+                mb.rooms.push(room);
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32, mb: &mut MapBuilder) {
+        use std::cmp::{max, min};
+        for y in min(y1, y2)..=max(y1, y2) {
+            if let Some(idx) = mb.map.try_idx(Point::new(x, y)) {
+                mb.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32, mb: &mut MapBuilder) {
+        use std::cmp::{max, min};
+        for x in min(x1, x2)..=max(x1, x2) {
+            if let Some(idx) = mb.map.try_idx(Point::new(x, y)) {
+                mb.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, rng: &mut RandomNumberGenerator, mb: &mut MapBuilder) {
+        let mut rooms = mb.rooms.clone();
+        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
+
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i - 1].center();
+            let new = room.center();
+
+            if rng.range(0, 2) == 1 {
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y, mb);
+                self.apply_vertical_tunnel(prev.y, new.y, new.x, mb);
+            } else {
+                self.apply_vertical_tunnel(prev.y, new.y, prev.x, mb);
+                self.apply_horizontal_tunnel(prev.x, new.x, new.y, mb);
+            }
+        }
+    }
+}
+
+impl MapArchitect for RoomsArchitect {
+    fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
+        let mut mb = MapBuilder {
+            map: Map::new(),
+            rooms: Vec::new(),
+            monster_spawns: Vec::new(),
+            player_start: Point::zero(),
+            amulet_start: Point::zero(),
+        };
+
+        mb.fill(TileType::Wall);
+        self.build_random_rooms(rng, &mut mb);
+        self.build_corridors(rng, &mut mb);
+        mb.player_start = mb.rooms[0].center();
+        mb.amulet_start = mb.find_most_distance();
+        for room in mb.rooms.iter().skip(1) {
+            mb.monster_spawns.push(room.center());
+        }
+        mb
+    }
+}
