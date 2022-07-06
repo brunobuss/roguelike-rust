076783f205466aef5f49ad2f210d0d0d9f64e1cc
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-06 15:31:19 +0100

    Add FoV

--- src/spawner.rs
+++ src/spawner.rs
@@ -9,9 +9,10 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             glyph: to_cp437('@'),
         },
         Health {
-            current: 20,
-            max: 20,
+            current: 10,
+            max: 10,
         },
+        FieldOfView::new(8),
     ));
 }
 
@@ -47,6 +48,7 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             max: hp,
         },
         Name(name),
+        FieldOfView::new(6),
     ));
 }
 
