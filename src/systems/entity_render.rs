6a0a79ae0d3e48f2412f45786a96182eaf827caf
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-13 15:12:24 +0100

    Render player after items

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -7,18 +7,27 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     let mut renderables = <(&Point, &Render)>::query();
-    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
-    let player_fov = fov.iter(ecs).next().unwrap();
+    let mut player = <(&FieldOfView, &Point, &Render)>::query().filter(component::<Player>());
+    let (player_fov, player_pos, player_render) = player.iter(ecs).next().unwrap();
 
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
     let offset = Point::new(camera.left_x, camera.top_y);
 
+    // Render everything but the player first.
     renderables
         .iter(ecs)
-        .filter(|(pos, _)| player_fov.visible_tiles.contains(pos))
+        .filter(|(pos, _)| player_fov.visible_tiles.contains(pos) && *pos != player_pos)
         .for_each(|(pos, render)| {
             draw_batch.set(*pos - offset, render.color, render.glyph);
         });
+
+    // Then draw the player, to avoid being "under" items in the screen.
+    draw_batch.set(
+        *player_pos - offset,
+        player_render.color,
+        player_render.glyph,
+    );
+
     draw_batch.submit(5000).expect("Batch error");
 }
