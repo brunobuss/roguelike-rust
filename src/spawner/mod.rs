750c1ac800802bbee9b0e7157c71e7ff17e649b5
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-14 16:24:08 +0100

    Add tests to spawner module

--- src/spawner/mod.rs
+++ src/spawner/mod.rs
@@ -44,3 +44,45 @@ pub fn spawn_level(
     let template = Templates::load();
     template.spawn_entities(ecs, resources, rng, level, spawn_points);
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+
+    #[test]
+    fn test_spawn_player() {
+        let mut ecs = World::default();
+        let pos = Point::new(10, 10);
+
+        spawn_player(&mut ecs, pos);
+        assert_eq!(ecs.len(), 1);
+
+        let player_entity = <Entity>::query().iter(&ecs).next().unwrap();
+        let player_entry = ecs.entry(*player_entity).unwrap();
+        assert!(player_entry.get_component::<Player>().is_ok());
+        assert!(player_entry.get_component::<Point>().is_ok());
+        assert_eq!(*player_entry.get_component::<Point>().unwrap(), pos);
+        assert!(player_entry.get_component::<Render>().is_ok());
+        assert!(player_entry.get_component::<Health>().is_ok());
+        assert!(player_entry.get_component::<FieldOfView>().is_ok());
+        assert!(player_entry.get_component::<Damage>().is_ok());
+    }
+
+    #[test]
+    fn test_spawn_amulet_of_yala() {
+        let mut ecs = World::default();
+        let pos = Point::new(10, 10);
+
+        spawn_amulet_of_yala(&mut ecs, pos);
+        assert_eq!(ecs.len(), 1);
+
+        let amulet_entity = <Entity>::query().iter(&ecs).next().unwrap();
+        let amulet_entry = ecs.entry(*amulet_entity).unwrap();
+        assert!(amulet_entry.get_component::<Item>().is_ok());
+        assert!(amulet_entry.get_component::<Point>().is_ok());
+        assert_eq!(*amulet_entry.get_component::<Point>().unwrap(), pos);
+        assert!(amulet_entry.get_component::<Render>().is_ok());
+        assert!(amulet_entry.get_component::<AmuletOfYala>().is_ok());
+        assert!(amulet_entry.get_component::<Name>().is_ok());
+    }
+}
