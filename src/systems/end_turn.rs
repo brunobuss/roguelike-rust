52ad847fe06ed081cddf8ee000a28925cda4cbb2
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-15 16:15:00 +0100

    Add tests to end_turn system

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
 
+/// This systems performs end of turns checks, in order to set the next state in the state machine.
 #[system]
 #[read_component(Health)]
 #[read_component(Point)]
@@ -34,4 +35,183 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState, #[resour
     });
 
     *turn_state = new_state;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn awaiting_input_stays() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let map = Map::new();
+        let player_pos = Point::new(10, 10);
+        resources.insert(map);
+        resources.insert(TurnState::AwaitingInput);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 10,
+                max: 10,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 1);
+        assert_eq!(
+            *resources.get::<TurnState>().unwrap(),
+            TurnState::AwaitingInput
+        );
+    }
+
+    #[test]
+    fn monster_turn_after_player_turn() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let map = Map::new();
+        let player_pos = Point::new(10, 10);
+        resources.insert(map);
+        resources.insert(TurnState::PlayerTurn);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 10,
+                max: 10,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 1);
+        assert_eq!(
+            *resources.get::<TurnState>().unwrap(),
+            TurnState::MonsterTurn
+        );
+    }
+
+    #[test]
+    fn awaiting_input_after_monster_turn() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let map = Map::new();
+        let player_pos = Point::new(10, 10);
+        resources.insert(map);
+        resources.insert(TurnState::MonsterTurn);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 10,
+                max: 10,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 1);
+        assert_eq!(
+            *resources.get::<TurnState>().unwrap(),
+            TurnState::AwaitingInput
+        );
+    }
+
+    #[test]
+    fn game_over_if_player_zero_health() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let map = Map::new();
+        let player_pos = Point::new(10, 10);
+        resources.insert(map);
+        resources.insert(TurnState::PlayerTurn);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 0,
+                max: 10,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 1);
+        assert_eq!(*resources.get::<TurnState>().unwrap(), TurnState::GameOver);
+    }
+
+    #[test]
+    fn game_win_if_player_found_amulet() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let map = Map::new();
+        let player_pos = Point::new(10, 10);
+        resources.insert(map);
+        resources.insert(TurnState::PlayerTurn);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 10,
+                max: 10,
+            },
+        ));
+        ecs.push((player_pos, AmuletOfYala {}));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 2);
+        assert_eq!(*resources.get::<TurnState>().unwrap(), TurnState::Victory);
+    }
+
+    #[test]
+    fn next_level_if_player_found_stairs() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder()
+            .add_system(super::end_turn_system())
+            .build();
+
+        let mut map = Map::new();
+        let player_pos = Point::new(10, 10);
+        let idx = map.point2d_to_index(player_pos);
+        map.tiles[idx] = TileType::Exit;
+        resources.insert(map);
+        resources.insert(TurnState::PlayerTurn);
+
+        ecs.push((
+            player_pos,
+            Player { map_level: 0 },
+            Health {
+                current: 10,
+                max: 10,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        assert_eq!(ecs.len(), 1);
+        assert_eq!(*resources.get::<TurnState>().unwrap(), TurnState::NextLevel);
+    }
 }
