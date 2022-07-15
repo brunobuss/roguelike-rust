9d15c2c44f78f96992d87654167a1ad7bb81cc77
Bruno Caricchio Buss <bruno.buss@gmail.com>
2022-07-15 15:52:58 +0100

    Add tests to fov system

--- src/systems/fov.rs
+++ src/systems/fov.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
 
+/// This system updates all FieldOfViews that are marked as dirty.
 #[system]
 #[read_component(Point)]
 #[write_component(FieldOfView)]
@@ -12,4 +13,70 @@ pub fn fov(ecs: &mut SubWorld, #[resource] map: &Map) {
             fov.visible_tiles = field_of_view_set(*pos, fov.radius, map);
             fov.is_dirty = false;
         });
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn clean_fov_is_not_updated() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder().add_system(super::fov_system()).build();
+
+        let map = Map::new();
+        resources.insert(map);
+
+        let e = ecs.push((
+            Point::new(10, 10),
+            FieldOfView {
+                visible_tiles: HashSet::new(),
+                radius: 1,
+                is_dirty: false,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        let entry = ecs.entry(e).unwrap();
+        let fov = entry.get_component::<FieldOfView>().unwrap();
+        assert_eq!(fov.is_dirty, false);
+        assert_eq!(fov.visible_tiles.is_empty(), true);
+    }
+
+    #[test]
+    fn dirty_fov_is_updated() {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        let mut sched = Schedule::builder().add_system(super::fov_system()).build();
+
+        let map = Map::new();
+        let center = Point::new(10, 10);
+        let mut expected_fov = HashSet::new();
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                let delta = Point::new(dx, dy);
+                let p = center + delta;
+                expected_fov.insert(p);
+            }
+        }
+
+        resources.insert(map);
+        let e = ecs.push((
+            center,
+            FieldOfView {
+                visible_tiles: HashSet::new(),
+                radius: 1,
+                is_dirty: true,
+            },
+        ));
+
+        sched.execute(&mut ecs, &mut resources);
+        let entry = ecs.entry(e).unwrap();
+        let fov = entry.get_component::<FieldOfView>().unwrap();
+        assert_eq!(fov.is_dirty, false);
+        assert_eq!(fov.visible_tiles, expected_fov);
+    }
 }
